c1f6588fdc709b59bd9554d6e1a16e35089829f2
mremondi <mremondi@colby.edu>
2017-02-04 14:24:17 -0500

    starting work

--- src/project0RemondiHughesHeCameron/HelloWorld.java
+++ src/project0RemondiHughesHeCameron/HelloWorld.java
@@ -1,15 +1,31 @@
 package project0RemondiHughesHeCameron;
 
-/**
+/***********
+ *
+ *
  * Created by mremondi on 2/4/17.
  */
 public class HelloWorld {
     public HelloWorld(){
 
+
+        String string = "{{{{{"/* { */;
+        String string1 = "{{{{/* { */";
+
+        char s = '{';
+
+        //////////{//{
+
     }
 
     public void helloWorld(){
-        System.out.println("hello world");
+        System.out.println("hello \"{  world");
+        /** { *
+         *
+         * {
+         */
+
+        // {{{{{{
     }
 
 }
--- src/project0RemondiHughesHeCameron/NontrivialBracesCounter.java
+++ src/project0RemondiHughesHeCameron/NontrivialBracesCounter.java
@@ -5,21 +5,98 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
-public class NontrivialBracesCounter {
+
+public class NontrivialBracesCounter{
+
+//    private enum characters{
+//        MULTILINECOMMENT, SINGLELINECOMMENT, STRING, CHAR
+//    }
+
+    private int leftBraceCount = 0;
+    private boolean counting = true;
+    private String ignoreTrivialToken = "";
 
     public NontrivialBracesCounter(){
 
     }
 
     public int getNumNontrivialLeftBraces(String program){
-        return 0;
+        for (int i=1; i<program.length(); i++){
+            char currentChar = program.charAt(i);
+            char lastChar = program.charAt(i-1);
+
+            if(counting) {
+                // count if this is true and not a new line
+//            if(currentChar == '\\') {
+//
+//            }
+
+                // multiline comment
+                if (currentChar == '*' && lastChar == '/') {
+                    // ignore everything until we find * /
+                    this.ignoreTrivialToken = "/*";
+                    this.counting = false;
+
+                }
+                // single line comment
+                else if (currentChar == '/' && lastChar == '/') {
+                    // ignore everything until we find \n
+                    this.ignoreTrivialToken = "//";
+                    this.counting = false;
+                }
+                // normal string
+                else if (currentChar == '"') {
+                    // ignore everything until we find another " unless there is breakout character
+                    this.ignoreTrivialToken = "\"";
+                    this.counting = false;
+
+                }
+                // string literal
+                else if (currentChar == '\'') {
+                    // ignore everything until we find '
+                    this.ignoreTrivialToken = "\'";
+                    this.counting = false;
+                }
+                else{
+                    if (currentChar == '{'){
+                        leftBraceCount++;
+                        System.out.println(i);
+                    }
+                }
+            }
+            else{
+                // multiline comment
+                if (currentChar == '/' && lastChar == '*' && ignoreTrivialToken.equals("/*")) {
+                    this.ignoreTrivialToken = "";
+                    this.counting = true;
+
+                }
+                // single line comment
+                else if (currentChar == '\n' && ignoreTrivialToken.equals("//")) {
+                    this.ignoreTrivialToken = "";
+                    this.counting = true;
+                }
+                // normal string
+                else if (currentChar == '"' && ignoreTrivialToken.equals("\"")) {
+                    this.ignoreTrivialToken = "";
+                    this.counting = true;
+                }
+                // string literal
+                else if (currentChar == '\'' && ignoreTrivialToken.equals("\'")) {
+                    this.ignoreTrivialToken = "";
+                    this.counting = true;
+                }
+            }
+        }
+        return leftBraceCount;
     }
 
     public static void main(String args[]){
         try {
             String contents = new Scanner(new File("HelloWorld.java") ).useDelimiter("\\Z").next();
             NontrivialBracesCounter counter = new NontrivialBracesCounter();
-            counter.getNumNontrivialLeftBraces(counter);
+            System.out.println(counter.getNumNontrivialLeftBraces(contents));
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
