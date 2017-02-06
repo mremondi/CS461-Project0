bbb1a883488f1066fffdcfb3e185bc19ee3b9ef9
nickcameron <ncameron@colby.edu>
2017-02-06 18:20:27 -0500

    Correct loop indexing to start at zero, move unnecessary field to method level, add javadocs

--- src/project0RemondiHughesHeCameron/NontrivialBracesCounter.java
+++ src/project0RemondiHughesHeCameron/NontrivialBracesCounter.java
@@ -1,22 +1,54 @@
+/*
+ * File: NontrivialBracesCounter.java
+ * Names: Nick Cameron, He He, Phoebe Hughes, Mike Remondi
+ * Class: CS 461
+ * Project 0
+ * Date: February 7
+ */
 package project0RemondiHughesHeCameron;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
+/**
+ * Provides a mechanism for counting the number of significant left braces: '{'
+ * in a valid Java program. 'Significant' here means that if the left brace were
+ * replaced with another character, the program would no longer compile.
+ */
 public class NontrivialBracesCounter{
 
+    /**
+     * Provides a set of modes for the traversal of a file, each used as a sentinel
+     * that determines that a character or collection of characters to be read is not
+     * significant, and more importantly what character or ordered collection of
+     * characters must be read before future characters regain significance.
+     */
     private enum TrivialCharacters{
         MULTILINECOMMENT, SINGLELINECOMMENT, STRING, CHAR, NONE
     }
 
-    private int leftBraceCount = 0;
+    /**
+     * Whether read characters are significant.
+     */
     private boolean counting = true;
+
+    /**
+     * Mode for determining how to enter or exit significance mode.
+     */
     private TrivialCharacters ignoreTrivialToken = TrivialCharacters.NONE;
 
+    /**
+     * Counts and returns the number of significant (defined in class JavaDoc) left
+     * braces in the given string.
+     *
+     * @param program   the string to count significant left braces from
+     * @return  the count of significant left braces
+     */
     private int getNumNontrivialLeftBraces(String program){
-        for (int i=1; i<program.length(); i++){
+        int leftBraceCount = 0;
+        counting = true;
+        for (int i=0; i<program.length(); i++){
             char currentChar = program.charAt(i);
             char lastChar = program.charAt(i-1);
             if(counting) {
